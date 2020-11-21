c66fc7f28ffeeee824f1e398ed9ca6e7a49cb922
codir1 <74709012+codir1@users.noreply.github.com>
2020-11-21 20:01:16 +0300

    Add files via upload

---
+++ precache-manifest.bc2965b78d73c56a7f1e3ee8560b2857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01813ab95b1fb221f9bccf490265bb92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a273e55925e2574f0720",
+    "url": "/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "a273e55925e2574f0720",
+    "url": "/static/js/2.30bbb83d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.30bbb83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885706ae8b2aaad2427d",
+    "url": "/static/js/main.865a5787.chunk.js"
+  },
+  {
+    "revision": "7454b842c22e9e7fa59d",
+    "url": "/static/js/runtime-main.50a7939c.js"
+  }
+]);
